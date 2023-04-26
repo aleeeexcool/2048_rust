c8118df797ad83d444129582de3295f9fabfe9b8
aleeeexcool <ramzhev.sasha@outlook.com>
2023-04-26 22:23:10 +0300

    first code

---
+++ target/debug/deps/hello_world-a2f994b283cce964.d
@@ -0,0 +1,5 @@
+/Users/aleeeexcool/hello_rust/hello_world/target/debug/deps/hello_world-a2f994b283cce964: src/main.rs
+
+/Users/aleeeexcool/hello_rust/hello_world/target/debug/deps/hello_world-a2f994b283cce964.d: src/main.rs
+
+src/main.rs:
---
+++ target/debug/hello_world.d
@@ -0,0 +1,1 @@
+/Users/aleeeexcool/hello_rust/hello_world/target/debug/hello_world: /Users/aleeeexcool/hello_rust/hello_world/src/main.rs
